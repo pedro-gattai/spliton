2d89998f8a2819acc3bd689410c094d0ef4df90f
Pedro Gattai <pedrogattai09@gmail.com>
2025-07-20 15:09:57 -0300

    fix: modules import

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { WalletModule } from './wallet/wallet.module';
+import { ExpensesModule } from './expenses/expenses.module';
 
 @Module({
   imports: [PrismaModule, WalletModule, ExpensesModule],
